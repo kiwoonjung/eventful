d6e23ce5fe5204c141f16000902696869fd828c5
kiwoonjung <kiwoon0627@gmail.com>
2023-04-03 01:35:42 -0700

    adding comments API routes

---
+++ pages/api/comments/[eventId].js
@@ -0,0 +1,46 @@
+function handler(req, res) {
+  const eventId = req.query.eventId;
+  if (req.method === "POST") {
+    const { email, name, text } = req.body;
+
+    if (
+      !email.includes("@") ||
+      !name ||
+      !name.trim() === "" ||
+      !text ||
+      !text.trim() === ""
+    ) {
+      res.status(422).json({ message: "Invaild input." });
+      return;
+    }
+
+    console.log(email, name, text);
+    const newComment = {
+      id: new Date().toISOString(),
+      email,
+      name,
+      text,
+    };
+    console.log(newComment);
+    res.status(201).json({ message: "Added comment", comment: newComment });
+  }
+
+  if (req.method === "GET") {
+    const dummyList = [
+      {
+        id: "c1",
+        name: "Max",
+        text: "A first comment!",
+      },
+      {
+        id: "c1",
+        name: "Manuel",
+        text: "A second comment!",
+      },
+    ];
+
+    res.status(200).json({ message: dummyList });
+  }
+}
+
+export default handler;
