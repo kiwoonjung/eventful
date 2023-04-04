b644113c803ef11777ef68bda2fec002f117458b
kiwoonjung <kiwoon0627@gmail.com>
2023-04-04 00:27:30 -0700

    adding error handing

--- pages/api/newsletter.js
+++ pages/api/newsletter.js
@@ -1,5 +1,19 @@
 import { MongoClient } from "mongodb";
 
+async function connectDatabase() {
+  const client = await MongoClient.connect(
+    "mongodb+srv://eventful:IDpPHmouG1reyW52@circlemarket-server.lmw8s6t.mongodb.net/eventful?retryWrites=true&w=majority"
+  );
+
+  return client;
+}
+
+async function insertDocument(client, document) {
+  const db = client.db();
+
+  await db.collection("newsletter").insertOne(document);
+}
+
 async function handler(req, res) {
   if (req.method === "POST") {
     const userEmail = req.body.email;
@@ -9,14 +23,22 @@ async function handler(req, res) {
       return;
     }
 
-    const client = await MongoClient.connect(
-      "mongodb+srv://eventful:IDpPHmouG1reyW52@circlemarket-server.lmw8s6t.mongodb.net/eventful?retryWrites=true&w=majority"
-    );
-    const db = client.db();
+    let client;
 
-    await db.collection("newsletter").insertOne({ email: userEmail });
+    try {
+      client = await connectDatabase();
+    } catch (error) {
+      res.status(500).json({ message: "Connecting to the database failed!" });
+      return;
+    }
 
-    client.close();
+    try {
+      await insertDocument(client, { email: userEmail });
+      client.close();
+    } catch (error) {
+      res.status(500).json({ message: "inserting data failed!" });
+      return;
+    }
 
     res.status(201).json({ message: "Sign Up!" });
   }
