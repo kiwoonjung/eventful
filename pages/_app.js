23e9b927f2a8436613cf250bd943383618578360
kiwoonjung <kiwoon0627@gmail.com>
2023-04-04 19:42:39 -0700

    creating a new react context

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,25 @@
+import Head from "next/head";
 import Layout from "../components/layout/layout";
+import { NotificationContextProvider } from "../store/notification-context";
 import "../styles/globals.css";
+import Notification from "../components/ui/notification";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <NotificationContextProvider>
+      <Layout>
+        <Head>
+          <title>Eventful</title>
+          <meta name="description" content="NextJS Eventful" />
+          <meta
+            name="viewport"
+            content="initial-scale=1.0, width=device-width"
+          />
+        </Head>
+        <Component {...pageProps} />
+        <Notification title="Test" message="This is a test." status="error" />
+      </Layout>
+    </NotificationContextProvider>
   );
 }
 
